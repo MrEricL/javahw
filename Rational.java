cca514f7c049f9b0907f36442f6228bdec2367f3
MrEricL <eli6@stuy.edu>
2016-12-06 10:59:28 -0500

    equals stuff

--- Rational.java
+++ Rational.java
@@ -74,20 +74,25 @@ public class Rational {
 	int p= a.denominator *denominator;
 	int q = a.numerator*denominator;
 	int g = numerator * a. denominator;
-	if (p==q) return 0;
-	else if (p>q) return  -1;
+	if (p==g) return 0;
+	else if (p>g) return  -1;
 	else return 1;
-
+    }
+    public boolean equals ( Object x){
+	Rational y    = (Rational) x;
+	if (y instanceof Rational && compareTo(y)==0) return true;
+	else throw new ClassCastException ("\n compareTo() input not a rational");
     }
     public static void main( String[] args )
     {
 	Rational r = new Rational(2,3); //Stores the rational number 2/3
 	Rational s = new Rational(1,2); //Stores the rational number 1/2
 	Rational t = new Rational(4,18); //Stores the rational number 4/18
-	r.add(s);  //Adds r to s, changes r to 7/6.  s remains 1/2
+	/*	r.add(s);  //Adds r to s, changes r to 7/6.  s remains 1/2
 	System.out.println(r);
 	System.out.println(s);
 	t.reduce(); //Changes t to 2/9
-	System.out.println(t);
+	System.out.println(t); */
+	System.out.println (r.compareTo(s));
     }
 }
