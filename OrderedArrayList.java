0f0b8a81995aee63bb367f4a66a16b438a33b9b4
MrEricL <eli6@stuy.edu>
2016-12-19 11:00:57 -0500

    new

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,14 +1,7 @@
 //Eric Li
 //APCS1 pd4
-//HW46 -- ascending
+//HW #48: Halving the Halves
 //2016-12-14
-
-/*============================================
-  class OrderedArrayList
-  Wrapper class for ArrayList.
-  Imposes the restriction that stored items 
-  must remain sorted in ascending order
-  ============================================*/
 
 //ArrayList's implementation is in the java.util package
 import java.util.ArrayList;
@@ -33,36 +26,6 @@ public class OrderedArrayList {
     public Comparable get( int index ) {
 	   return _data.get(index);
     }
-    // addLinear takes as input any comparable object 
-    // (i.e., any object of a class implementing interface Comparable)
-    // inserts newVal at the appropriate index
-    // maintains ascending order of elements
-    // uses a linear search to find appropriate index
-
-    /* --------------
-_data = [1,2,4]
-new val = 3
-
-_data is not empty so skip this check
-
-      index 0 = 1
-      1 compareTo 3 -> -1
-      
-LOOP
-
-     index 1 = 2
-     2 compareTo 3 -> -1
-
-LOOP
-
-     index 2 = 4
-     2 compareTo 4 -> 1
-     push the 4 to index 3
-     index 2 becomes 3
-
-
-
-    -----------------  */
     
     public void addLinear( Comparable newVal ) {
 	if (_data.isEmpty()) _data.add(newVal); //checks if its empty 
@@ -78,20 +41,23 @@ LOOP
 	    }
 	}
     }
+    public  void addBinary(){
+	OrderedArrayList copy = game;
+
+    }
     // main method solely for testing purposes
     public static void main( String[] args ) {
-	OrderedArrayList Franz = new OrderedArrayList();
+	OrderedArrayList game = new OrderedArrayList();
 	System.out.println("\nValues to add via addLinear() calls:");
 	// testing linear search
-	for( int i = 0; i < 15; i++ ) {
-	    int valToAdd = (int)( 50 * Math.random() );
-	    System.out.println( valToAdd );
-	    Franz.addLinear( valToAdd );
+	for( int i = 0; i < ((int)(10*Math.random())); i++ ) {
+	    int valToAdd = (int)( 100 * Math.random() );
+	    game.addLinear( valToAdd );
 	}
-	System.out.println("\nafter population via addLinear() calls:");
-	System.out.println( Franz );
-	System.out.println();
-
+	System.out.println( game );
+	System.out.println( game.size() );
+	addBinary();
+	System.out.println( game );
     }
 
 }//end class OrderedArrayList
