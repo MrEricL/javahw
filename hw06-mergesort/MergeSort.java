5a328f8b32e02befb6d0afd27718d483e78f8f1f
MrEricL <eli6@stuy.edu>
2017-02-11 20:21:47 -0500

    new

--- hw06-mergesort/MergeSort.java
+++ hw06-mergesort/MergeSort.java
@@ -1,8 +1,18 @@
 /*======================================
+Eric Li
+APCS2 pd3
+HW#06: Step 1: Split, Step 2: ?, Step 3: Sorted!. . .
+2017-02-13
+
+
   class MergeSort
   Implements mergesort on array of ints.
 
   Summary of Algorithm: 
+
+The sort method returns the given array if it's length one, since the program needs to break up the data into one. Then it creates two arrays which are half of the given array. The loops fill it up with the respective numbers. Then it recusrivly sort and then merges. 
+
+TL;DR: Breaks array into array with length one recusively and then merges it from back up
 
   ======================================*/
 
@@ -55,9 +65,19 @@ public class MergeSort{
      ******************************************************/
     public static int[] sort( int[] arr ) 
     {
-	return arr;
+	if (arr.length <= 1){
+	    return arr;
+	}
 
-    }//end sort()
+	    int m = arr.length;
+	    int[] a = new int[arr.length/2];
+	    int[] b = new int [arr.length-a.length];	      
+	    for (int i = 0; i < a.length; i++) a[i] = arr[i];
+	    for (int i = 0; i < b.length; i++) b[i] = arr[i+m/2];
+
+	    return merge(sort(a),sort(b));	
+
+    }
 
 
 
@@ -82,13 +102,14 @@ public class MergeSort{
     //main method for testing
     public static void main( String [] args ) {
 
+	/*
 	int[] b = {0,1,2,3};
 	int[] g = {1,2,4};
 	int[] z = merge(b,g);
 	printArray(z);
-	    
+	*/
 
-	/*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
+
 	int[] arr0 = {0};
 	int[] arr1 = {1};
 	int[] arr2 = {1,2};
@@ -114,6 +135,7 @@ public class MergeSort{
 	printArray( sort( arr5 ) );
 	printArray( sort( arr6 ) );
 	printArray( sort( arr7 ) );
+	/*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~	
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     }//end main()
 
